b26078097ccc48281ca6c147f26c3e52c75609d8
Luxary147 <ividueirab@fpcoruna.afundacion.org>
2022-11-29 23:20:44 +0100

    Cambiados temas por defecto y eliminacion  de plugins por default

---
+++ MortyWorld/wp-content/themes/neve/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/neve',
+        'pretty_version' => 'v3.4.6',
+        'version' => '3.4.6.0',
+        'reference' => '645d9e010c44f733601ba8b1eaac0e0da4fa7f9a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/neve' => array(
+            'pretty_version' => 'v3.4.6',
+            'version' => '3.4.6.0',
+            'reference' => '645d9e010c44f733601ba8b1eaac0e0da4fa7f9a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.2.31',
+            'version' => '3.2.31.0',
+            'reference' => '04ab7589bf32dbfddbf66bbb69bb0ab2eb8d7c27',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wptt/webfont-loader' => array(
+            'pretty_version' => 'v1.1.3',
+            'version' => '1.1.3.0',
+            'reference' => '19fa29ba8d82bed018fb8c6f949799739c61356c',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../wptt/webfont-loader',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
